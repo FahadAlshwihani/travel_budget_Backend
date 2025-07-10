b693d1d44e7a03a7a951fcc37990c37874ac2642
Fahad  Alshwihani <fhdalshammri34@gmail.com>
2025-07-10 21:19:22 +0300

    Delete travel_budget_tracker/travel_budget_tracker directory

--- travel_budget_tracker/travel_budget_tracker/travel_budget_tracker/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),  # Admin panel URL
-    path('api/', include('tracker.urls')),  # Include URLs from the tracker app
-
-]
